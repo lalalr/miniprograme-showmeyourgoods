608506ef4c3d741b896d2c24785fcc9ed52b88b7
lalalr <ohkamism@gmail.com>
2020-07-11 18:09:27 +0800

    push

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,51 +5,25 @@ const app = getApp()
 Page({
   data: {
     select: false,
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-		canIUse: wx.canIUse('button.open-type.getUserInfo'),
-		
-		currentTab:0
-  },
-  //事件处理函数
-
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    goodslist:[{
+        catid:"1",
+        itemlist:[{
+            id:"1",
+            name:"男装 轻型腰包 423485423485423485",
+            desc:"123456",
+            ischosen:false
+        }]
+      },
+      {
+        catid:"2",
+        itemlist:[{
+          id:"1",
+          name:"女装 轻型腰包 423485423485423485",
+          desc:"abcde",
+          ischosen:false
+        }]
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    ]
   },
 
   selectTap: function(){
